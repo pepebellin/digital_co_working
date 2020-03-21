dfac502b0192462ecb958455f0ed7c580072cd5b
Pepe Bellin <pepebellin@gmail.com>
2020-03-21 23:05:28 +0100

    react init

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
-const port = 3000;
+const port = 5000;
 
 // Where we will keep books
 let books = [];
