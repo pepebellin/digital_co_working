9b0d4f584277e36ce8d901c378801564cf46b76d
Pepe Bellin <pepebellin@gmail.com>
2020-03-22 02:09:17 +0100

    c

--- frontend/react_app/src/Teams.js
+++ frontend/react_app/src/Teams.js
@@ -1,17 +1,23 @@
 import React from 'react';
-import '../css/registerCopy.css';
+import './index.css';
 
 function Teams() {
-  return (
-    <div class="group4" style="float: left; position: initial;">
-        <img anima-src="../img/register-copy-rectangle-8-copy-2@2x.png" class="rectangle8copy2" src="data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw=="/>
-        <div class="team1marketing">
-            <span class="span1">Team 1</span>
-            <span class="span2"><br />Marketing</span>
-        </div>
-        <img anima-src="../img/register-copy-group@2x.png" class="group" src="data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw=="/>
-    </div>
-  );
+    var content = [];
+    var teamsJson = {"teams":[{"team_name":"Team1"},{"team_name":"Team2"},{"team_name":"Team3"},{"team_name":"Team4"}]}; 
+
+    var i;
+    for (i = 0; i < teamsJson.teams.length; i++) {
+        content.push(
+            <div class="group4" style={{float: "left", position: "initial"}}>
+                <img alt="" anima-src="../img/register-copy-rectangle-8-copy-2@2x.png" class="rectangle8copy2" src="data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw=="/>
+                <div class="team1marketing">
+                    <span class="span1">{teamsJson.teams[i].team_name}</span>
+                </div>
+                <img alt="" anima-src="../img/register-copy-group@2x.png" class="group" src="data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw=="/>
+            </div>)
+    }
+
+    return content;
 }
 
 export default Teams;
