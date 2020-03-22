f5951eef56ccc28d9eb235c5e7c2afa3e3de7de7
Pepe Bellin <pepebellin@gmail.com>
2020-03-22 02:40:12 +0100

    v

--- frontend/react_app/src/Teams.js
+++ frontend/react_app/src/Teams.js
@@ -8,12 +8,12 @@ function Teams() {
     var i;
     for (i = 0; i < teamsJson.teams.length; i++) {
         content.push(
-            <div class="group4" style={{float: "left", position: "initial"}}>
+            <div class="group4" style={{position: "static", float: "left", paddingLeft: "50px"}}>
                 <img alt="" anima-src="../img/register-copy-rectangle-8-copy-2@2x.png" class="rectangle8copy2" src="data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw=="/>
                 <div class="team1marketing">
-                    <span class="span1">{teamsJson.teams[i].team_name}</span>
+                    <span style={{paddingRight:"90px;"}} class="span1">{teamsJson.teams[i].team_name}</span>
                 </div>
-                <img alt="" anima-src="../img/register-copy-group@2x.png" class="group" src="data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw=="/>
+                <img  alt="" anima-src="../img/register-copy-group@2x.png" class="group" src="data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw=="/>
             </div>)
     }
 
